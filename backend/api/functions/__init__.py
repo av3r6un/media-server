c8294fff91b8fe57d56366c44b8cdfb9f87f26c2
av3r6un <av3rgun@yandex.com>
2023-11-14 13:22:32 +0300

    Fixing import problems

--- backend/api/functions/__init__.py
+++ backend/api/functions/__init__.py
@@ -1,6 +1,5 @@
 from .parse_item import parse_item
 from .voices import Voices
 from .parse_meta import MetaEngine
-from .convertion import InternalConverter
 from .media_engine import MediaEngine
 from .advice_engine import AdviceEngine
